4ef6bf1d48cb7ab5764d672c1b800b9512f42178
Erica Roquemore <enroquemore@gmail.com>
2022-06-08 07:02:23 -0400

    props added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
+function Hello(props) {
+  console.log(Object.keys(props));
+  return (
+    <div>
+      <h1>Welcome to {props.library}!</h1>
+      <p>{props.message}</p>
+      <p>How many number of props is {props.number}</p>
+      <p>{Object.keys(props).length} Props Total</p>
+    </div>
+  );
+}
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <ul>
-    <li>Hot Dogs</li>
-    <li>Hamburgers</li>
-    <li>Pizza</li>
-    <li>Sushi</li>
-  </ul>
-);
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Hello 
+  library="Vue" 
+  message="Have fun!" 
+  number={3} />);
