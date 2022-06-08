6223c67ff6280ae5378d8d33674b59292a9f260a
Erica Roquemore <enroquemore@gmail.com>
2022-06-08 08:08:04 -0400

    rendering lists of objects

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const lakeList = [
-  "Lake Juliette", 
-  "Erica Lake", 
-  "Lady Lake"
+  { id: "1", name: "Echo", trailhead: "Echo" },
+  { id: "2", name: "Erica", trailhead: "Rocks" },
+  { id: "3", name: "Juliette", trailhead: "Bayview" }
 ];
 
 function App({ lakes }) {
    return (
-     <ul>
-       {lakes.map(lake => <li>{lake}</li>)}
-     </ul>
+     <div>
+       {lakes.map(lake => (
+         <div>
+           <h2>{lake.name}</h2>
+           <p>Accessed by: {lake.trailhead}</p>
+           </div>
+       ))}
+     </div>
     );
 }
 
