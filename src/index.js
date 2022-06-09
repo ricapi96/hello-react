872793c5008026a1bbffa3a3bde5bf662756c7e8
Erica Roquemore <enroquemore@gmail.com>
2022-06-08 22:15:03 -0400

    adding keys

--- src/index.js
+++ src/index.js
@@ -2,26 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const lakeList = [
-  { id: "1", name: "Echo", trailhead: "Echo" },
-  { id: "2", name: "Erica", trailhead: "Rocks" },
-  { id: "3", name: "Juliette", trailhead: "Bayview" }
-];
 
-function App({ lakes }) {
+const list = [1, 2, 3, 4, 5];
+
+function App({ items }) {
    return (
-     <div>
-       {lakes.map(lake => (
-         <div>
-           <h2>{lake.name}</h2>
-           <p>Accessed by: {lake.trailhead}</p>
-           </div>
+     <ul>
+       {items.map(item => (
+         <li key={item.toString()}>{item}</li>
        ))}
-     </div>
+     </ul>
     );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <App lakes={lakeList}/>
+  <App items={list}/>
 );
